bf2b1b8521be7c63f79aab2251f357d6a375d72a
LIYI <9830131@qq.com>
2022-12-25 15:46:17 +0800

    2022-12-25_15:46

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -1,20 +1,33 @@
 import { navbar } from "vuepress-theme-hope";
 
 export const zhNavbar = navbar([
-  "/",
-  { text: "文章", link: "/posts.html" },
-  { text: "标签", link: "/tags.html" },
-  { text: "分类", link: "/categories.html" },
-  { text: "归档", link: "/archives.html" },
-  { text: "友情链接", link: "/friendlinks.html" },
-  { text: "留言", link: "/guestbook.html" },
-  { text: "关于作者", link: "/about.html" },
-  // {
-  //   text: "电子书",
-  //   icon: "book",
-  //   prefix: "/",
-  //   children: [
-  //     { text: "《TS PBL一本通》", link: "ts/" },
-  //   ],
-  // },
+  {
+    text: "首页",
+    icon: "home",
+    link: "/",
+  },
+  {
+    text: "博客",
+    // icon: "blog",
+    // prefix: "https://yishulun.com",
+    link: "https://yishulun.com",
+    children: [
+      {text: "文章", link: "/posts.html"},
+      {text: "标签", link: "/tags.html"},
+      {text: "分类", link: "/categories.html"},
+      {text: "归档", link: "/archives.html"},
+      {text: "友情链接", link: "/friendlinks.html"},
+      {text: "留言", link: "/guestbook.html"},
+      {text: "联系作者", link: "/about.html"},
+    ],
+  },
+  {
+    text: "电子书",
+    // icon: "read",
+    prefix: "https://yishulun.com",
+    children: [
+      // {text: "TS PBL一本通", link: "/ts/"},
+      {text: "给初学者看的Web开发教程", link: "/Web-Dev-For-Beginners-CN/"},
+    ],
+  },
 ]);
