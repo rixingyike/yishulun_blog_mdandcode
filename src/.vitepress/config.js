c1e9c5e36400a3ca5d8a857511ac42013beba5f9
LIYI <9830131@qq.com>
2023-07-08 21:27:28 +0800

    2023-07-08_21:27

--- src/.vitepress/config.js
+++ src/.vitepress/config.js
@@ -6,12 +6,17 @@ export default {
   base: '/',
   lang: 'zh-CN',
   title: '艺述论博客',
-  description: '80后，社恐+老码农一枚，有20年以上上网经历。',
+  description: '80后，小社恐+老码农一枚，有20年以上上网经历。',
   srcDir: ".",
   lastUpdated: true,
   ignoreDeadLinks: true,
 
   themeConfig: {
+    returnToTopLabel: "Return to top",
+    editLink: {
+      pattern: 'https://github.com/rixingyike/rixingyike.github.io/edit/main/src/:path',
+      text: 'Edit on GitHub'
+    },
     footer: {
       message: '<a href="https://beian.miit.gov.cn/" target="_blank">京ICP备第14007000号-7号</a>，由<a href="https://vitepress.dev/" target="_blank">VitePress 1.0 beta5</a>驱动。',
       copyright: 'Copyright © 2002-2023 LIYI'
@@ -25,6 +30,7 @@ export default {
       { text: '博客', link: '/posts/' },
       { text: '文档', link: '/docs/' },
       { text: '关于', link: '/about.html' },
+      { text: 'Github', link: 'https://github.com/rixingyike' },
     ],
     sidebar: generateSidebar({
       rootGroupText: 'Contents',
