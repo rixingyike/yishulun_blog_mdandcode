1588200bf97c5989f3f0ff870fffad28b7ce7bec
LIYI <9830131@qq.com>
2023-07-22 09:43:01 +0800

    2023-07-22_09:43

--- src/.vitepress/config.js
+++ src/.vitepress/config.js
@@ -17,6 +17,14 @@ export default {
       {},
       "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-8N64Q25EK9');",
     ],
+    [
+      'script',
+      {
+        async: true,
+        src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9936052907746352',
+        crossorigin: "anonymous",
+      },
+    ],
   ],
   // site-level options
   base: '/',
