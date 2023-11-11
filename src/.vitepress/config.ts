1abcd067c54917d898681cd8a1e3dd2a1e24cfc2
LIYI <9830131@qq.com>
2023-11-11 09:16:25 +0800

    2023-11-11:

--- src/.vitepress/config.ts
+++ src/.vitepress/config.ts
@@ -82,7 +82,7 @@ async function config() {
       docsDir: "./src/docs",
       // docsBranch: "master",
       posts: await getPosts(),
-      pageSize: 7,
+      pageSize: 10,
       postLength: await getPostLength(),
       nav: [
         {
