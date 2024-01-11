3b46ff66315d494facb47ea42b829e62899a0266
LIYI <9830131@qq.com>
2024-01-11 23:53:05 +0800

    2024-01-11_23:53

--- src/.vitepress/theme/components/Award.vue
+++ src/.vitepress/theme/components/Award.vue
@@ -6,6 +6,7 @@
     <p class="description">
       <a href="https://item.jd.com/13728755.html" target="_blank"><img style="max-width: 300px;" src="/wegame-book.png" /></a>
     </p>
+
     <h1 class="title">
       <span>Courses</span>
     </h1>
@@ -15,13 +16,12 @@
     <p class="description">
       <a href="http://gk.link/a/10AdC" target="_blank"><img style="max-width: 400px" src="/weapp-optimize.png" /></a>
     </p>
+
     <h1 class="title">
       <span>Links</span>
     </h1>
     <p class="description">
-      <ul>
-        <li><a href="/posts/2023/02.html" target="_blank">《微信小游戏开发》勘误列表</a></li>
-      </ul>
+      <a href="/posts/2023/02.html" target="_blank">《微信小游戏开发》勘误列表</a>
     </p>
   </div>
 </template>
