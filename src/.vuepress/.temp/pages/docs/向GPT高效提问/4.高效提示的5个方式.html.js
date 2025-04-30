1b3c5bff3bf011cf147c531072b7b637130b09d4
李艺 <9830131@qq.com>
2025-04-30 15:21:32 +0800

    自动提交: 2025-04-30_15:21:32

--- src/.vuepress/.temp/pages/docs/向GPT高效提问/4.高效提示的5个方式.html.js
+++ src/.vuepress/.temp/pages/docs/向GPT高效提问/4.高效提示的5个方式.html.js
@@ -1,5 +1,5 @@
 import comp from "C:/Users/Admin/work/yishulun_blog_mdandcode/src/.vuepress/.temp/pages/docs/向GPT高效提问/4.高效提示的5个方式.html.vue"
-const data = JSON.parse("{\"path\":\"/docs/%E5%90%91GPT%E9%AB%98%E6%95%88%E6%8F%90%E9%97%AE/4.%E9%AB%98%E6%95%88%E6%8F%90%E7%A4%BA%E7%9A%845%E4%B8%AA%E6%96%B9%E5%BC%8F.html\",\"title\":\"4.高效提示的5个方式\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"4.高效提示的5个方式\",\"createTime\":\"2025/02/19 13:58:17\",\"gitInclude\":[]},\"headers\":[],\"readingTime\":{\"minutes\":5.38,\"words\":1613},\"filePathRelative\":\"docs/向GPT高效提问/4.高效提示的5个方式.md\",\"categoryList\":[{\"id\":\"e3e2a9\",\"sort\":10000,\"name\":\"docs\"},{\"id\":\"549859\",\"sort\":10006,\"name\":\"向GPT高效提问\"}]}")
+const data = JSON.parse("{\"path\":\"/docs/%E5%90%91GPT%E9%AB%98%E6%95%88%E6%8F%90%E9%97%AE/4.%E9%AB%98%E6%95%88%E6%8F%90%E7%A4%BA%E7%9A%845%E4%B8%AA%E6%96%B9%E5%BC%8F.html\",\"title\":\"4.高效提示的5个方式\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"4.高效提示的5个方式\",\"createTime\":\"2025/02/19 13:58:17\",\"gitInclude\":[]},\"headers\":[],\"readingTime\":{\"minutes\":5.38,\"words\":1613},\"filePathRelative\":\"docs/向GPT高效提问/4.高效提示的5个方式.md\",\"categoryList\":[{\"id\":\"e3e2a9\",\"sort\":10000,\"name\":\"docs\"},{\"id\":\"549859\",\"sort\":10008,\"name\":\"向GPT高效提问\"}]}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
--- src/.vuepress/.temp/pages/docs/向GPT高效提问/index.html.js
+++ src/.vuepress/.temp/pages/docs/向GPT高效提问/index.html.js
@@ -1,5 +1,5 @@
 import comp from "C:/Users/Admin/work/yishulun_blog_mdandcode/src/.vuepress/.temp/pages/docs/向GPT高效提问/index.html.vue"
-const data = JSON.parse("{\"path\":\"/docs/%E5%90%91GPT%E9%AB%98%E6%95%88%E6%8F%90%E9%97%AE/\",\"title\":\"向 GPT 高效提问\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"向 GPT 高效提问\",\"gitInclude\":[]},\"headers\":[],\"readingTime\":{\"minutes\":0.58,\"words\":174},\"filePathRelative\":\"docs/向GPT高效提问/README.md\",\"categoryList\":[{\"id\":\"e3e2a9\",\"sort\":10000,\"name\":\"docs\"},{\"id\":\"549859\",\"sort\":10006,\"name\":\"向GPT高效提问\"}]}")
+const data = JSON.parse("{\"path\":\"/docs/%E5%90%91GPT%E9%AB%98%E6%95%88%E6%8F%90%E9%97%AE/\",\"title\":\"向 GPT 高效提问\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"向 GPT 高效提问\",\"gitInclude\":[]},\"headers\":[],\"readingTime\":{\"minutes\":0.58,\"words\":174},\"filePathRelative\":\"docs/向GPT高效提问/README.md\",\"categoryList\":[{\"id\":\"e3e2a9\",\"sort\":10000,\"name\":\"docs\"},{\"id\":\"549859\",\"sort\":10008,\"name\":\"向GPT高效提问\"}]}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
